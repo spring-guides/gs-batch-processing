ee95d7cbbbf692053431951c73b8b57dfda84da5
Greg Turnquist <gturnquist@gopivotal.com>
2013-05-16 11:12:00 -0400

    Initial commit of the working, complete example.
    This one has trickier issues to overcome, so I thought it more practical to
    push the code to origin so I could ping other people, before having to worry
    about writing the actual guide.

---
+++ complete/src/main/java/hello/BatchConfiguration.java
@@ -0,0 +1,94 @@
+package hello;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder().
+				addScript("classpath:org/springframework/batch/core/schema-drop-hsqldb.sql").
+				addScript("classpath:org/springframework/batch/core/schema-hsqldb.sql").
+				addScript("classpath:person.sql").
+				build();
+	}
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
+	
+	@Bean
+	public ItemReader<Person> reader() {
+		FlatFileItemReader<Person> reader = new FlatFileItemReader<Person>();
+		reader.setResource(new ClassPathResource("sample-data.csv"));
+		reader.setLineMapper(new DefaultLineMapper<Person>() {{
+			setLineTokenizer(new DelimitedLineTokenizer(){{
+				setNames(new String[]{"firstName", "lastName"});
+			}});
+			setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>(){{
+				setTargetType(Person.class);
+			}});
+		}});
+		return reader;
+	}
+	
+	@Bean
+	public ItemProcessor<Person, Person> processor() {
+		return new PersonItemProcessor();
+	}
+
+	@Bean
+	public ItemWriter<Person> writer(DataSource dataSource) {
+		JdbcBatchItemWriter<Person> writer = new JdbcBatchItemWriter<Person>();
+		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Person>());
+		writer.setSql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)");
+		writer.setDataSource(dataSource);
+		return writer;
+	}
+	
+	@Bean
+	public Job importUserJob(JobBuilderFactory jobs, Step s1) {
+		Job job = jobs.get("importUserJob")
+				.incrementer(new RunIdIncrementer())
+				.flow(s1)
+				.end()
+				.build();
+		return job;
+	}
+	
+	@Bean
+	public Step step1(StepBuilderFactory stepBuilderFactory, ItemReader<Person> reader, 
+			ItemWriter<Person> writer, ItemProcessor<Person, Person> processor) {
+		return stepBuilderFactory.get("step1")
+				.<Person,Person> chunk(10)
+				.reader(reader)
+				.processor(processor)
+				.writer(writer)
+				.build();
+	}
+	
+}
