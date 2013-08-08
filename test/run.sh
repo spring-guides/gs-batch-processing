82b2bed8805bdd7c3cbe19ec1ae6fb4e23243a48
Roy Clarkson <rclarkson@gopivotal.com>
2013-08-07 23:17:45 -0400

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,13 +1,36 @@
 #!/bin/sh
 cd $(dirname $0)
+
 cd ../complete
+
 mvn clean package
-rm -rf target
-./gradlew build
-rm -rf build
-cd ../initial
-mvn clean package
-rm -rf target
-./gradlew build
-rm -rf build
+ret=$?
+if [ $ret -ne 0 ]; then
 exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
+cd ../initial
+
+mvn clean package
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
+exit
