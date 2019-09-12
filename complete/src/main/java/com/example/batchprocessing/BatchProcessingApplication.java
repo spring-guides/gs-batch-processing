4cfd7fee9e172b79d8aaac337b92d304fc95e180
Jay Bryant <jbryant@pivotal.io>
2019-09-12 09:20:15 -0500

    Use the Spring Initializr
    I rewrote the doc and rearranged the code to use the Spring Initializr.

--- complete/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
+++ complete/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
@@ -1,12 +1,12 @@
-package hello;
+package com.example.batchprocessing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class BatchProcessingApplication {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(BatchProcessingApplication.class, args);
     }
 }
--- complete/src/main/java/com/example/batchprocessing/Person.java
+++ complete/src/main/java/com/example/batchprocessing/Person.java
@@ -1,4 +1,4 @@
-package hello;
+package com.example.batchprocessing;
 
 public class Person {
 
