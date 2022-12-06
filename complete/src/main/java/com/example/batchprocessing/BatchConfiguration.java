2d5e526be662cd3430c527f1de0b90d0f2e9b91b
Robert McNees <rmcnees@vmware.com>
2022-12-06 07:47:15 -0500

    Updated documentation for Batch 5 and Boot 3 upgrade

--- complete/src/main/java/com/example/batchprocessing/BatchConfiguration.java
+++ complete/src/main/java/com/example/batchprocessing/BatchConfiguration.java
@@ -19,11 +19,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
-// tag::setup[]
 @Configuration
 public class BatchConfiguration {
-
-	// end::setup[]
 
 	// tag::readerwriterprocessor[]
 	@Bean
