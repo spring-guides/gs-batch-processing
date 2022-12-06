ed2604bfa5b8140e0b98e0c1bd8f307e85ce2979
Robert McNees <rmcnees@vmware.com>
2022-12-06 07:27:57 -0500

    Updated code for Batch 5 and Boot 3

--- complete/src/main/java/com/example/batchprocessing/BatchConfiguration.java
+++ complete/src/main/java/com/example/batchprocessing/BatchConfiguration.java
@@ -3,36 +3,26 @@ package com.example.batchprocessing;
 import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
 
 // tag::setup[]
 @Configuration
-@EnableBatchProcessing
 public class BatchConfiguration {
 
-	@Autowired
-	public JobBuilderFactory jobBuilderFactory;
-
-	@Autowired
-	public StepBuilderFactory stepBuilderFactory;
 	// end::setup[]
 
 	// tag::readerwriterprocessor[]
@@ -66,8 +56,9 @@ public class BatchConfiguration {
 
 	// tag::jobstep[]
 	@Bean
-	public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
-		return jobBuilderFactory.get("importUserJob")
+	public Job importUserJob(JobRepository jobRepository,
+			JobCompletionNotificationListener listener, Step step1) {
+		return new JobBuilder("importUserJob", jobRepository)
 			.incrementer(new RunIdIncrementer())
 			.listener(listener)
 			.flow(step1)
@@ -76,9 +67,10 @@ public class BatchConfiguration {
 	}
 
 	@Bean
-	public Step step1(JdbcBatchItemWriter<Person> writer) {
-		return stepBuilderFactory.get("step1")
-			.<Person, Person> chunk(10)
+	public Step step1(JobRepository jobRepository,
+			PlatformTransactionManager transactionManager, JdbcBatchItemWriter<Person> writer) {
+		return new StepBuilder("step1", jobRepository)
+			.<Person, Person> chunk(10, transactionManager)
 			.reader(reader())
 			.processor(processor())
 			.writer(writer)
