19bab2c8d8e89df61da82e82e81ec298831b0194
Philippe Soares <philippe.soares@vizgen.com>
2023-01-20 21:18:29 -0500

    refactor(JobCompletionNotificationListener): replace deprecated parent class by interface
    I'm also fixing a log statement to avoid string concatenation with `+`.
    
    Fixes: #48

--- complete/src/main/java/com/example/batchprocessing/JobCompletionNotificationListener.java
+++ complete/src/main/java/com/example/batchprocessing/JobCompletionNotificationListener.java
@@ -4,13 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
+public class JobCompletionNotificationListener implements JobExecutionListener {
 
 	private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
@@ -30,7 +30,7 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
 				(rs, row) -> new Person(
 					rs.getString(1),
 					rs.getString(2))
-			).forEach(person -> log.info("Found <" + person + "> in the database."));
+			).forEach(person -> log.info("Found <{{}}> in the database.", person));
 		}
 	}
 }
